c1c67ffab1e2d240388b313cb4ba75f96af57fc5
Danilo Novaes <danilo.novaes@hotmail.com.br>
2021-10-16 17:19:19 -0300

    StartProjeto
    Criado projeto

---
+++ cadastro-login/src/test/java/br/com/impacta/CadastroLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.impacta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
