fa244ae9ecff8f6396ff01558050f7e42f1c6b0a
Danilo Novaes <danilo.novaes@hotmail.com.br>
2021-11-28 11:54:40 -0300

    Campanha Microserviço
    Foi criado a estrutura da campanha de microserviço.
    Falta colocar as funcionalidades

---
+++ campanha/src/main/java/br/com/impacta/configuration/OpenApiConfiguration.java
@@ -0,0 +1,29 @@
+package br.com.impacta.configuration;
+
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+
+
+@OpenAPIDefinition(info = @Info(title = "campanha" , version = "v1",
+description = "documentação de API campanha"))
+public class OpenApiConfiguration {
+	
+	@Bean
+	public OpenAPI customOpenApi() {
+		return new OpenAPI()
+				.components(new Components())
+				.info(
+						new io.swagger.v3.oas.models.info.Info()
+						.title("campanha API")
+						.version("v1")
+						.license( new License().name("Apache 2.0").url("http://springdoc.org")));
+				
+		
+	}
+
+}
