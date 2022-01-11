37a46e2616a10c374f2a61b7d215d6e4b7b7ce22
Danilo Novaes <51716610+induscabos02@users.noreply.github.com>
2022-01-11 10:53:23 -0300

    atualizacao
    atualizado o cors

--- cadastro-login/src/main/java/br/com/impacta/configuration/CorsConfiguration.java
+++ cadastro-login/src/main/java/br/com/impacta/configuration/CorsConfiguration.java
@@ -1,9 +1,11 @@
 package br.com.impacta.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 	
 	 @Override
